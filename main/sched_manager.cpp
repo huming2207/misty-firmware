cac440ffdd99222436822213867885a397daadd5
Jackson Ming Hu <huming2207@gmail.com>
2025-11-22 22:44:31 +1100

    sched_manager: rework

---
+++ main/sched_manager.cpp
@@ -0,0 +1,197 @@
+#include "sched_manager.hpp"
+
+#include "esp_log.h"
+
+esp_err_t sched_manager::init()
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // NVS partition was truncated and needs to be erased
+        // Retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    ret = nvs_open("cron", NVS_READWRITE, &nvs);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "init: can't open NVS: 0x%x", ret);
+        return ret;
+    }
+
+    // We don't use NVS functionality provided by ESP schedule because it can't save additional info
+    // Instead we do it on our on, so that we can save whatever we want!
+    esp_schedule_init(false, nullptr, nullptr);
+    dispatch_queue = xQueueCreate(3, sizeof(void *));
+    if (dispatch_queue == nullptr) {
+        ESP_LOGE(TAG, "init: can't create dispatch queue");
+        return ESP_ERR_NO_MEM;
+    }
+
+    if (xTaskCreate(schedule_dispatch_task, "cron_dispatch", 4096, this, tskIDLE_PRIORITY + 3, nullptr) != pdPASS) {
+        ESP_LOGE(TAG, "init: can't create dispatch task");
+        return ESP_ERR_NO_MEM;
+    }
+
+    return load_schedules();
+}
+
+esp_err_t sched_manager::load_schedules()
+{
+    nvs_iterator_t nvs_it = nullptr;
+    esp_err_t ret = nvs_entry_find_in_handle(nvs, NVS_TYPE_BLOB, &nvs_it);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "load_sched: Can't open NVS handle: 0x%x", ret);
+        return ret;
+    }
+
+    for (size_t idx = 0; idx < task_items.size(); idx += 1) {
+        if (task_items[idx].scheduler != nullptr) {
+            esp_schedule_disable(task_items[idx].scheduler);
+            esp_schedule_delete(task_items[idx].scheduler);
+        }
+
+        memset(&task_items[idx], 0, sizeof(cron_task_item));
+    }
+
+    xQueueReset(dispatch_queue);
+
+    size_t item_idx = 0;
+    while (nvs_it != nullptr && item_idx < task_items.size()) {
+        nvs_entry_info_t entry = {};
+        ret = nvs_entry_info(nvs_it, &entry);
+        if (ret != ESP_OK) {
+            ESP_LOGW(TAG, "load_sched: can't load NVS entry: 0x%x", ret);
+            return ret;
+        }
+
+        cron_store_entry item = {};
+        size_t item_size = 0;
+        ret = nvs_get_blob(nvs, entry.key, &item, &item_size);
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG, "load_sched: Can't read item %s, returned 0x%x", entry.key, ret);
+            return ret;
+        }
+
+        if (item_size < sizeof(item)) {
+            ESP_LOGE(TAG, "load_sched: corrupted item with wrong size %u want %u", item_size, sizeof(item));
+
+            ret = nvs_entry_next(&nvs_it);
+            if (ret != ESP_OK) {
+                break;
+            }
+        }
+
+        strncpy(task_items[item_idx].name, entry.key, sizeof(cron_task_item::name) - 1);
+        task_items[item_idx].name[sizeof(cron_task_item::name) - 1] = '\0';
+
+        memcpy(&task_items[item_idx].sched_info, &item, sizeof(item));
+
+        esp_schedule_config_t sched_cfg = {};
+        strncpy(sched_cfg.name, entry.key, sizeof(cron_task_item::name) - 1);
+        sched_cfg.name[sizeof(cron_task_item::name) - 1] = '\0';
+
+        sched_cfg.priv_data = &task_items[item_idx];
+        sched_cfg.validity.end_time = 0;
+        sched_cfg.validity.start_time = 0;
+        sched_cfg.trigger_cb = schedule_trigger_callback;
+        sched_cfg.trigger.day.repeat_days = task_items[item_idx].sched_info.day_of_week;
+        sched_cfg.trigger.type = task_items[item_idx].sched_info.schedule_type;
+        if (sched_cfg.trigger.type == ESP_SCHEDULE_TYPE_DAYS_OF_WEEK) {
+            sched_cfg.trigger.hours = task_items[item_idx].sched_info.dow.hour;
+            sched_cfg.trigger.minutes = task_items[item_idx].sched_info.dow.minute;
+        } else if (sched_cfg.trigger.type == ESP_SCHEDULE_TYPE_SUNRISE || sched_cfg.trigger.type == ESP_SCHEDULE_TYPE_SUNSET) {
+            sched_cfg.trigger.solar.offset_minutes = task_items[item_idx].sched_info.offset_minute;
+        } else {
+            ESP_LOGE(TAG, "Unsupported schedule type %u", sched_cfg.trigger.type);
+
+            ret = nvs_entry_next(&nvs_it);
+            if (ret != ESP_OK) {
+                break;
+            }
+
+            continue;
+        }
+
+        task_items[item_idx].scheduler = esp_schedule_create(&sched_cfg);
+        if (!task_items[item_idx].scheduler) {
+            ESP_LOGE(TAG, "load: can't create scheduler");
+            return ESP_ERR_NO_MEM; // Maybe reboot instead??
+        }
+        esp_schedule_enable(task_items[item_idx].scheduler);
+        ESP_LOGI(TAG, "load: inserted item %s, type=%u", task_items[item_idx].name, task_items[item_idx].sched_info.schedule_type);
+
+        ret = nvs_entry_next(&nvs_it);
+        item_idx += 1;
+        if (ret != ESP_OK || nvs_it == nullptr || item_idx >= task_items.size()) {
+            break;
+        }
+    }
+
+    ESP_LOGI(TAG, "load_sched: done, got %u schedules", item_idx);
+    return ESP_OK;
+}
+
+esp_err_t sched_manager::set_schedule(const char* name, const cron_store_entry* entry)
+{
+    size_t cfg_len = sizeof(cron_store_entry);
+    esp_err_t ret = nvs_get_blob(nvs, name, nullptr, &cfg_len);
+    if (ret == ESP_ERR_NVS_NOT_FOUND || ret == ESP_ERR_NOT_FOUND) {
+        ESP_LOGI(TAG, "set: item %s inserting", name);
+        ret = nvs_set_blob(nvs, name, entry, sizeof(cron_store_entry));
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG, "set: failed to insert: 0x%x", ret);
+            return ret;
+        }
+
+        return load_schedules();
+    }
+
+    if (ret == ESP_OK) {
+        ESP_LOGW(TAG, "set: item %s already exist", nvs);
+        return ESP_ERR_INVALID_STATE;
+    } else {
+        ESP_LOGE(TAG, "set: NVS error 0x%x", ret);
+    }
+
+    return ret;
+}
+
+esp_err_t sched_manager::get_schedule(const char* name, cron_store_entry* entry_out) const
+{
+    size_t len = sizeof(cron_store_entry);
+    esp_err_t ret = nvs_get_blob(nvs, name, &entry_out, &len);
+    return ret;
+}
+
+esp_err_t sched_manager::delete_schedule(const char* name) const
+{
+    return nvs_erase_key(nvs, name);
+}
+
+void sched_manager::schedule_dispatcher(cron_task_item* item)
+{
+
+}
+
+void sched_manager::schedule_dispatch_task(void* _ctx)
+{
+    auto &mgr = instance();
+
+    while (true) {
+        cron_task_item *task_item = nullptr;
+        if (xQueueReceive(mgr.dispatch_queue, &task_item, portMAX_DELAY) != pdTRUE) {
+            ESP_LOGW(TAG, "dispatch_task: nothing to receive??");
+            vTaskDelay(1);
+            return;
+        }
+
+        mgr.schedule_dispatcher(task_item);
+        vTaskDelay(1);
+    }
+}
+
+void sched_manager::schedule_trigger_callback(esp_schedule_handle_t handle, void* ctx) // ctx is the item!!
+{
+    auto &mgr = instance();
+    xQueueSend(mgr.dispatch_queue, ctx, portMAX_DELAY);
+}
--- main/sched_manager.hpp
+++ main/sched_manager.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <deque>
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
 #include <esp_schedule.h>
 
 #include "nvs_flash.h"
@@ -9,23 +11,31 @@
 
 #include "esp_bit_defs.h"
 
-class cron_manager
+class sched_manager
 {
 public:
-    static cron_manager &instance()
+    static sched_manager &instance()
     {
-        static cron_manager _instance;
+        static sched_manager _instance;
         return _instance;
     }
 
-    cron_manager(cron_manager const &) = delete;
-    void operator=(cron_manager const &) = delete;
+    sched_manager(sched_manager const &) = delete;
+    void operator=(sched_manager const &) = delete;
 
     enum pump_bits : uint8_t
     {
         PUMP_0 = BIT(0),
         PUMP_1 = BIT(1),
         PUMP_ALL = PUMP_0 | PUMP_1,
+    };
+
+    enum duration_profile : uint8_t
+    {
+        PROFILE_DRY = 0,
+        PROFILE_MODERATE = 1,
+        PROFILE_WET = 2,
+        PROFILE_COUNT,
     };
 
     struct __attribute__((packed)) cron_store_entry
@@ -41,7 +51,7 @@ public:
             } dow;
             int16_t offset_minute;
         };
-        uint32_t duration_ms;
+        uint32_t duration_ms[PROFILE_COUNT];
         esp_schedule_type_t schedule_type;
     };
 
@@ -54,13 +64,21 @@ public:
 
     esp_err_t init();
     esp_err_t load_schedules();
+    esp_err_t set_schedule(const char *name, const cron_store_entry *entry);
+    esp_err_t get_schedule(const char *name, cron_store_entry *entry_out) const;
+    esp_err_t delete_schedule(const char *name) const;
 
 private:
-    cron_manager() = default;
+    sched_manager() = default;
+    void schedule_dispatcher(cron_task_item *item);
+    static void schedule_dispatch_task(void *_ctx);
     static void schedule_trigger_callback(esp_schedule_handle_t handle, void *ctx);
 
     nvs_handle_t nvs = 0;
-    std::deque<cron_task_item> task_items = {};
+    QueueHandle_t dispatch_queue = nullptr;
+
+    // Because I'm targeting ESP32-C6 so better off use array instead of vector/deque to save heap
+    std::array<cron_task_item, 10> task_items = {};
 
 
     static const constexpr char TAG[] = "cronman";
