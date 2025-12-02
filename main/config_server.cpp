938ea18e8e9e09e6e1ca7649adb7e3924318e7a6
Jackson Ming Hu <huming2207@gmail.com>
2025-12-02 21:47:20 +0800

    config_server: implement get schedule handler

--- main/config_server.cpp
+++ main/config_server.cpp
@@ -1,6 +1,9 @@
+#include <esp_log.h>
+#include <esp_app_desc.h>
 #include "config_server.hpp"
 
-#include "esp_log.h"
+#include "mjson.h"
+#include "sched_manager.hpp"
 
 esp_err_t config_server::init()
 {
@@ -11,14 +14,6 @@ esp_err_t config_server::init()
         ESP_LOGE(TAG, "init: can't start httpd");
         return ret;
     }
-
-    httpd_uri_t list_all_schedules_cfg = {
-        .uri = "/api/schedule/list",
-        .method = HTTP_GET,
-        .handler = list_all_schedule_handler,
-        .user_ctx = this,
-    };
-    ret = httpd_register_uri_handler(httpd, &list_all_schedules_cfg);
 
     httpd_uri_t add_schedule_cfg = {
         .uri = "/api/schedule",
@@ -48,6 +43,27 @@ esp_err_t config_server::init()
         ESP_LOGE(TAG, "init: can't register handlers: 0x%x", ret);
     }
 
+    httpd_uri_t set_wifi_handler = {
+        .uri = "/api/wifi",
+        .method = HTTP_POST,
+        .handler = set_wifi_config_handler,
+        .user_ctx = this,
+    };
+    ret = ret ?: httpd_register_uri_handler(httpd, &set_wifi_handler);
+
+    httpd_uri_t get_fw_handler = {
+        .uri = "/api/fwinfo",
+        .method = HTTP_GET,
+        .handler = get_firmware_info_handler,
+        .user_ctx = this,
+    };
+    ret = ret ?: httpd_register_uri_handler(httpd, &get_fw_handler);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "init: can't register handlers: 0x%x", ret);
+    }
+
+
     return ret;
 }
 
@@ -58,22 +74,109 @@ esp_err_t config_server::stop()
     return ret;
 }
 
-esp_err_t config_server::list_all_schedule_handler(httpd_req_t* req)
-{
-    return ESP_OK;
-}
-
 esp_err_t config_server::get_schedule_handler(httpd_req_t* req)
 {
-    return ESP_OK;
+    httpd_resp_set_type(req, "application/json");
+
+    char query[64] = { 0 };
+    char out[152] = { 0 };
+    if (httpd_req_get_url_query_len(req) > sizeof(query) - 1 || httpd_req_get_url_query_len(req) <= 1) {
+        sched_manager::instance().list_all_schedule_names_to_json(out, sizeof(out));
+        return httpd_resp_send(req, out, (ssize_t)strnlen(out, sizeof(out)));
+    }
+
+    if (httpd_req_get_url_query_str(req, query, sizeof(query) - 1) != ESP_OK) {
+        sched_manager::instance().list_all_schedule_names_to_json(out, sizeof(out));
+        return httpd_resp_send(req, out, (ssize_t)strnlen(out, sizeof(out)));
+    }
+
+    char name[17] = { 0 };
+    auto ret = httpd_query_key_value(query, "name", name, sizeof(name) - 1);
+    name[sizeof(name) - 1] = '\0';
+
+    sched_manager::cron_store_entry entry = {};
+    ret = ret ?: sched_manager::instance().get_schedule(name, &entry);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get schedule: 0x%x", ret);
+        return httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Failed to get schedule");
+    }
+
+    int len = 0;
+    if (entry.schedule_type == ESP_SCHEDULE_TYPE_DAYS_OF_WEEK) {
+        len = snprintf(out, sizeof(out), R"({"name":"%s","pump":%u,"dow":%u,"h":%u,"m":%u,"duration":[%lu,%lu,%lu],"type":%u})",
+            name, entry.select_pumps, entry.day_of_week, entry.dow.hour, entry.dow.minute,
+            entry.duration_ms[sched_manager::PROFILE_DRY], entry.duration_ms[sched_manager::PROFILE_MODERATE],
+            entry.duration_ms[sched_manager::PROFILE_WET], (uint8_t)entry.schedule_type);
+    } else if (entry.schedule_type == ESP_SCHEDULE_TYPE_SUNRISE || entry.schedule_type == ESP_SCHEDULE_TYPE_SUNSET) {
+        len = snprintf(out, sizeof(out), R"({"name":"%s","pump":%u,"dow":%u,"offset":%d,"duration":[%lu,%lu,%lu],"type":%u})",
+           name, entry.select_pumps, entry.day_of_week, entry.offset_minute,
+           entry.duration_ms[sched_manager::PROFILE_DRY], entry.duration_ms[sched_manager::PROFILE_MODERATE],
+           entry.duration_ms[sched_manager::PROFILE_WET], (uint8_t)entry.schedule_type);
+    } else {
+        ESP_LOGE(TAG, "Invalid schedule type %u (probably corrupted?)", entry.schedule_type);
+        return httpd_resp_send_err(req, HTTPD_505_VERSION_NOT_SUPPORTED, "Invalid schedule type");
+    }
+
+    if (len < 1) {
+        ESP_LOGE(TAG, "Can't format output JSON");
+        return httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Can't format output JSON");
+    }
+
+    return httpd_resp_send(req, out, (ssize_t)strnlen(out, sizeof(out)));
 }
 
 esp_err_t config_server::add_schedule_handler(httpd_req_t* req)
 {
+    httpd_resp_set_type(req, "application/json");
+    auto *ctx = (config_server *)req->user_ctx;
+    if (ctx != nullptr) {
+        return httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Invalid user context");
+    }
+
+
+
     return ESP_OK;
 }
 
 esp_err_t config_server::remove_schedule_handler(httpd_req_t* req)
 {
+    httpd_resp_set_type(req, "application/json");
+    auto *ctx = (config_server *)req->user_ctx;
+    if (ctx != nullptr) {
+        return httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Invalid user context");
+    }
     return ESP_OK;
+}
+
+esp_err_t config_server::set_wifi_config_handler(httpd_req_t* req)
+{
+    httpd_resp_set_type(req, "application/json");
+    auto *ctx = (config_server *)req->user_ctx;
+    if (ctx != nullptr) {
+        return httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Invalid user context");
+    }
+    return ESP_OK;
+}
+
+esp_err_t config_server::get_wifi_config_handler(httpd_req_t* req)
+{
+    httpd_resp_set_type(req, "application/json");
+    auto *ctx = (config_server *)req->user_ctx;
+    if (ctx != nullptr) {
+        return httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Invalid user context");
+    }
+    return ESP_OK;
+}
+
+esp_err_t config_server::get_firmware_info_handler(httpd_req_t* req)
+{
+    httpd_resp_set_type(req, "application/json");
+
+    char out[192] = { 0 };
+    int len = snprintf(out, sizeof(out), R"({"sdk":"%s","fw":"%s","compDate":"%s"})", IDF_VER, esp_app_get_description()->version, esp_app_get_description()->date);
+    if (len < 2) {
+        return httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Invalid firmware info");
+    }
+
+    return httpd_resp_send(req, out, len);
 }
--- main/config_server.hpp
+++ main/config_server.hpp
@@ -2,6 +2,8 @@
 
 #include <esp_http_server.h>
 
+#include "nvs.h"
+
 class config_server
 {
 public:
@@ -10,11 +12,14 @@ public:
 
 private:
     static void noop_free(void *_ptr) {} // Bypass the auto free() for the context pointer
-    static esp_err_t list_all_schedule_handler(httpd_req_t *req);
     static esp_err_t get_schedule_handler(httpd_req_t *req);
     static esp_err_t add_schedule_handler(httpd_req_t *req);
     static esp_err_t remove_schedule_handler(httpd_req_t *req);
+    static esp_err_t set_wifi_config_handler(httpd_req_t *req);
+    static esp_err_t get_wifi_config_handler(httpd_req_t *req);
+    static esp_err_t get_firmware_info_handler(httpd_req_t *req);
     httpd_handle_t httpd = nullptr;
+    nvs_handle_t nvs = 0; // Not to be confused with scheduler's NVS - this is for misc configs (e.g. WiFi)
 
 private:
     static constexpr char TAG[] = "cfg_server";
